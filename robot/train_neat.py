05a2c9fe8c837cdb9e3a28ef82ce258584c58af3
Michael Perju <michael@this.one>
2023-12-03 10:50:36 +0000

    update

--- robot/train_neat.py
+++ robot/train_neat.py
@@ -29,7 +29,7 @@ def evaluate_genomes(genomes, config):
         neural_network = neat.nn.FeedForwardNetwork.create(genome, config)
 
         wandb.init(
-            project="spot-neat-1000x1000 generations",
+            project="spot-neat-1024x1000 generations (2)",
             config=dict(
                 genome_id=genome_id,
                 config=str(config)
@@ -60,39 +60,39 @@ def run_neat():
     population.add_reporter(reporter)
     population.add_reporter(neat.StatisticsReporter())
     population.add_reporter(neat.Checkpointer(
-        generation_interval=50,
+        generation_interval=1,
         time_interval_seconds=1800,  #  30 minutes
         filename_prefix=f'{logs_dir}/')
     )
 
     # Run NEAT evolution
     best_genome = population.run(evaluate_genomes, 1000)
-    # Create a neural network based on the best genome
-    best_neural_network = neat.nn.FeedForwardNetwork.create(
-        best_genome, config)
+    # # Create a neural network based on the best genome
+    # best_neural_network = neat.nn.FeedForwardNetwork.create(
+    #     best_genome, config)
 
-    # Initialize SpotEnvironment with rendering enabled
-    spot_env = SpotEnvironment(
-        steps_per_episode=10000, goal_distance=10, should_render=True)
+    # # Initialize SpotEnvironment with rendering enabled
+    # spot_env = SpotEnvironment(
+    #     steps_per_episode=10000, goal_distance=10, should_render=True)
 
-    # Initialize Agent with the winning policy
-    agent = Agent(
-        env=spot_env,
-        policy=best_neural_network,
-        n_episodes=10000,
-        steps_before_learning=50,
-        checkpoint_episode_interval=101,
-    )
+    # # Initialize Agent with the winning policy
+    # agent = Agent(
+    #     env=spot_env,
+    #     policy=best_neural_network,
+    #     n_episodes=10000,
+    #     steps_before_learning=50,
+    #     checkpoint_episode_interval=101,
+    # )
 
-    # Interact with the environment using the winning policy
-    agent.interact()
+    # # Interact with the environment using the winning policy
+    # agent.interact()
 
-    # Retrieve and print information about the interaction
-    reached_distance = spot_env.data.body("trunk").xpos[0]
-    max_distance = spot_env.max_distance
+    # # Retrieve and print information about the interaction
+    # reached_distance = spot_env.data.body("trunk").xpos[0]
+    # max_distance = spot_env.max_distance
 
-    print("Reached distance:", reached_distance)
-    print("Max distance:", max_distance)
+    # print("Reached distance:", reached_distance)
+    # print("Max distance:", max_distance)
 
 
 run_neat()
